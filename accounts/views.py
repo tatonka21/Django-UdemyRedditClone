09b05e31c588eb5bb26b64a811c38ad89907a24c
Ruiqi <iamruiqi@gmail.com>
2017-12-23 13:39:05 -0500

    created accounts redirect and posts template

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
@@ -12,7 +12,7 @@ def signup(request):
 
             try:
                 user = User.objects.get(username=request.POST['username'])
-                return render(request,'accounts/signup.html', {'error': 'Username has already been taken.'})
+                return render(request,'accounts/signup.html', {'error': 'eUsername has already been taken.'})
             except User.DoesNotExist:
                 user = User.objects.create_user(request.POST['username'],password=request.POST['password1'])
                 login(request,user)
@@ -32,6 +32,9 @@ def loginview(request):
         if user is not None:
             login(request, user)
             # Redirect to a success page.
+            if 'next' in request.POST:
+                if request.POST['next'] is not None:
+                    return redirect(request.POST['next'])
             return render(request,'accounts/login.html', {'error': 'Login Success!'})
         else:
             return render(request,'accounts/login.html', {'error': 'The Username and Password didn\'t match'})
