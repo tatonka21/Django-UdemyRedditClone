763e9e34d6aea3ddfa5e6a845060be70d22b7d42
Ruiqi <iamruiqi@gmail.com>
2017-12-26 15:48:59 -0500

    created upvote and downvote features

--- posts/views.py
+++ posts/views.py
@@ -12,7 +12,10 @@ def create(request):
         if request.POST['title'] and request.POST['url']:
             post = Post()
             post.title = request.POST['title']
-            post.url =  request.POST['url']
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                post.url =  request.POST['url']
+            else:
+                post.url = 'http://' + request.POST['url']
             post.pub_date = timezone.datetime.now()
             post.author = request.user
             post.save()
@@ -25,3 +28,17 @@ def create(request):
 def home(request):
     posts = Post.objects.order_by('votes_total')
     return render(request, 'posts/home.html',{'posts':posts})
+
+def upvote(request, pk):
+    if request.method == "POST":
+        post = Post.objects.get(pk=pk)
+        post.votes_total += 1
+        post.save()
+        return redirect('home')
+
+def downvote(request, pk):
+    if request.method == "POST":
+        post = Post.objects.get(pk=pk)
+        post.votes_total -= 1
+        post.save()
+        return redirect('home')
